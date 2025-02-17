a41327eb59abc8f0a4ad148bf6fc76ca1d575d0a
bulanovk <bulanovk@gmail.com>
2025-02-17 14:42:44 +0400

    Add UI Flow

--- custom_components/ecto_modbus/__init__.py
+++ custom_components/ecto_modbus/__init__.py
@@ -112,7 +112,7 @@ async def async_setup_entry(hass: HomeAssistant, entry) -> bool:
     await setup_modbus_server(hass, config, devices)
 
     # Регистрация платформ
-    await hass.config_entries.async_forward_entry_setup(entry, "switch")
+    await hass.config_entries.async_forward_entry_setups(entry, "switch")
     return True
 
 async def setup_modbus_server(hass, config, devices):
